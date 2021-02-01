ced7ff3596178123bab8a54a440903b380892c5e
AMAN <amanarora17077@gmail.com>
2021-02-01 20:15:21 +0530

    authentication system

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const PORT =process.env.PORT || 3300;
 const mongoose =require('mongoose');
 const session =require('express-session');
 const flash =require('express-flash');
-const MongoDbStore =require('connect-mongo')(session)
+const MongoDbStore =require('connect-mongo')(session);
+const passport =require('passport');
 
 const url = 'mongodb://localhost/pizza';
 mongoose.connect(url, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
@@ -17,8 +18,12 @@ const connection = mongoose.connection;
 connection.once('open', () => {
     console.log('Database connected...');
 }).catch(err => {
-    console.log('Connection failed...')
+    console.log('Connection failed...');
 });
+
+
+
+
 
 //session store
 let  mongoStore = new MongoDbStore({
@@ -36,14 +41,23 @@ app.use(session({
    
 }))
 
+//passport config
+const passportInit =require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
+
 app.use(flash());
 //Assets
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
 //global middleware
 app.use((req,res,next)=>{
     res.locals.session =req.session;
+    res.locals.user =req.user;
     next();
 })
 
