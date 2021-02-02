d21e63236e5c1d2303c61ebb2bc50aa9d85dcdb2
AMAN <amanarora17077@gmail.com>
2021-02-03 02:11:23 +0530

    add order tables

--- app/http/controllers/customers/orderController.js
+++ app/http/controllers/customers/orderController.js
@@ -1,19 +1,38 @@
-const order = require("../../../models/order")
-
 const Order = require('../../../models/order')
-
-function orderController() {
+const moment = require('moment')
+function orderController () {
     return {
-        index(req, res) {
-           order.find({ status: { $ne: 'completed' } }, null, { sort: { 'createdAt': -1 }}).populate('customerId', '-password').exec((err, orders) => {
-               if(req.xhr) {
-                   return res.json(orders)
-               } else {
-                return res.render('admin/orders')
-               }
-           })
+        store(req, res) {
+            // Validate request 
+            const { phone, address } = req.body
+            if(!phone || !address) {
+                req.flash('error', 'All fields are required')
+                return res.redirect('/cart')
+            }
+
+            const order = new Order({
+                customerId: req.user._id,
+                items: req.session.cart.items,
+                phone,
+                address
+            })
+            order.save().then(result => {
+                req.flash('success', 'Order placed successfully')
+                delete req.session.cart
+                return res.redirect('/customer/orders')
+            }).catch(err => {
+                req.flash('error', 'Something went wrong')
+                return res.redirect('/cart')
+            })
+        },
+        async index(req, res) {
+            const orders = await Order.find({ customerId: req.user._id }, 
+                null, 
+                { sort: { 'createdAt': -1 } } )
+            res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0')
+            res.render('customers/orders', { orders: orders, moment: moment })
         }
     }
 }
 
-module.exports = orderController  
+module.exports = orderController
--- routes/web.js
+++ routes/web.js
@@ -1,8 +1,14 @@
 const authController = require('../app/http/controllers/authController');
 const homeController = require('../app/http/controllers/homeController');
 const cartController = require('../app/http/controllers/customers/cartController');
+const orderController = require('../app/http/controllers/customers/orderController');
+const guest =require('../app/http/middleware/guest');
+const auth =require('../app/http/middleware/auth'); //middleware
+const admin =require('../app/http/middleware/admin') //middleware
+const AdminOrderController = require('../app/http/controllers/admin/orderController');
 
-const guest =require('../app/http/middleware/guest')
+
+
 function initRoutes(app) {
 
 
@@ -17,8 +23,12 @@ function initRoutes(app) {
     app.get('/cart', cartController().index)
     app.post('/update-cart', cartController().update)
 
-   
+    app.post('/orders',auth, orderController().store)
+    app.get('/customer/orders',auth, orderController().index)
 
+        //admin routes
+
+    app.get('/admin/orders',admin,AdminOrderController().index)
 
 }
 
